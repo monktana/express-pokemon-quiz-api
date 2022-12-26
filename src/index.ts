e8c016032c929efa0bcd81dcb271927868f6f37b
Sven Giebelmann <sven@giebelmann.dev>
2022-12-26 14:16:04 +0100

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import type { Request, Response } from "express"
+
+const express = require('express')
+const app = express();
+const port = 3000
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
